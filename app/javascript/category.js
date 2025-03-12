38484ff20966898c55a8ddfab68cf6f406186ea9
kazyu0 <kazumakimijima.sb@gmail.com>
2025-03-12 14:17:34 +0900

    子カテゴリー

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -6,8 +6,25 @@ window.addEventListener('turbo:load', function () {
 
   }
 
+  const XHR = new XMLHttpRequest();
+  const categoryXHR = (id) => {
+    XHR.open("GET", `/category/${id}`, true);
+    XHR.responseType = "json";
+    XHR.send();
+  }
+
+  const getChildCategoryData = () => {
+    const parentValue = parentCategory.value
+    categoryXHR(parentValue)
+
+    XHR.onload = () => {
+      const items = XHR.response.item;
+    }
+  }
+
   parentCategory.addEventListener('change', function () {
     selectChildElement('child-select-wrap')
+    getChildCategoryData()
   })
 
 })
